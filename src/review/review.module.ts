0b8ed9a66caae567fa3c111f35d7cb334dc50331
Ilya Zakharov <74999345+Suppolyar@users.noreply.github.com>
2024-01-19 19:34:53 +0700

    feat: Connection models

--- src/review/review.module.ts
+++ src/review/review.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ReviewController } from './review.controller';
+import { TypegooseModule } from 'nestjs-typegoose';
+import { ReviewModel } from './review.model/review.model';
 
 @Module({
   controllers: [ReviewController],
+  imports: [
+    TypegooseModule.forFeature([
+      {
+        typegooseClass: ReviewModel,
+        schemaOptions: { collection: 'Review' },
+      },
+    ]),
+  ],
 })
 export class ReviewModule {}
--- src/top-page/top-page.module.ts
+++ src/top-page/top-page.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TopPageController } from './top-page.controller';
 import { ConfigModule } from '@nestjs/config';
+import { TypegooseModule } from 'nestjs-typegoose';
+import { TopPageModel } from './top-page.model/top-page.model';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [
+    ConfigModule,
+    TypegooseModule.forFeature([
+      {
+        typegooseClass: TopPageModel,
+        schemaOptions: { collection: 'TopPage' },
+      },
+    ]),
+  ],
   controllers: [TopPageController],
 })
 export class TopPageModule {}
