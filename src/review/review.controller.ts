073bdb0cb5c4080bde957ecb753a9d9019f0fb5c
Ilya Zakharov <74999345+Suppolyar@users.noreply.github.com>
2024-01-22 17:03:19 +0700

    feat: added a guards to auth module

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -7,12 +7,15 @@ import {
   HttpStatus,
   Param,
   Post,
+  UseGuards,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { ReviewService } from './review.service';
 import { REVIEW_NOT_FOUND } from './review.constants';
+import { JwtAuthGuard } from '../auth/guards/jwt.guard';
+import { Types } from 'mongoose';
 
 @Controller('review')
 export class ReviewController {
@@ -24,6 +27,7 @@ export class ReviewController {
     return await this.reviewService.create(dto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   async delete(@Param('id') id: string) {
     const deletedDoc = await this.reviewService.delete(id);
